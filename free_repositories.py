dafcb6023b182a82a0978352d06d1aaa798d44d0
Ciznia <gabriel.hosquet@epitech.eu>
2023-06-17 00:23:29 +0200

    :recycle: Refactor all for safety product and beautifulness

--- free_repositories.py
+++ free_repositories.py
@@ -1,73 +1,96 @@
+from __future__ import annotations
+
 import os
 import json
 import sys
-from typing import Final
+import pathlib
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Final, TypedDict, List, Callable
 
-PATH: Final[str] = "/data/dev/Ciztech"
 
-REPO_LIST_PATH: Final[str] = f"{PATH}/repo_list.json"
-REPO_PATH: Final[str] = f"{PATH}/repos"
-TEMPLATE_PATH: Final[str] = f"{PATH}/template"
+PATH: Final[Path] = pathlib.Path.cwd()
+
+REPO_LIST_PATH: Final[Path] = PATH / "repo_list.json"
+REPO_PATH: Final[Path] = PATH / "repos"
+TEMPLATE_PATH: Final[Path] = PATH / "template"
 
 ORIGIN_KEY: Final[str] = "origin"
 MIRROR_KEY: Final[str] = "mirror"
 PATH_KEY: Final[str] = "path"
 
 
-def clone(mirror, origin, path):
-    folder = mirror.split("/")
-    if len(folder) != 2:
-        return
-    _, name = folder
-    name = name[:-4]
-    if not os.path.exists(name):
-        os.system(f"git clone {mirror} {path}/{name} --recursive")
-        os.chdir(f"{path}/{name}")
-        if origin:
-            os.system(f"git remote add epitech {origin}")
-    else:
-        os.chdir(f"{path}/{name}")
-    if origin:
-        os.system("git pull epitech main")
-    else:
-        os.system("git pull")
-    if not (os.path.exists(".gitattributes") or "stumper_" in name):
-        os.system(f"cp {TEMPLATE_PATH}/.gitattributes .")
-    os.system("git push origin main")
-    count = f"{path}/{name}".count('/')
-    os.chdir("/".join((".." for _ in range (count + 1))))
+class RepoInfo(TypedDict):
+    path: str
+    origin: str
+    mirror: str
 
 
-def all_mirror(content):
-    for line in content:
-        origin = line.get(ORIGIN_KEY)
-        mirror = line.get(MIRROR_KEY)
-        path = line.get(PATH_KEY)
-        clone(mirror, origin, path)
+@dataclass
+class Repo:
+    path: Path
+    origin: str
+    mirror: str
 
+    @property
+    def name(self) -> str:
+        folder = self.mirror.split("/")
 
-def specific_mirror(content):
-    for line in content:
-        origin = line.get(ORIGIN_KEY)
-        mirror = line.get(MIRROR_KEY)
-        path = line.get(PATH_KEY)
-        folder = mirror.split("/")
         if len(folder) != 2:
-            return
+            return "Unknown"
+
         _, name = folder
-        name = name[:-4]
-        if name in sys.argv:
-            clone(mirror, origin, path)
+        return name[:-4]
+
+    @classmethod
+    def from_dict(cls, data_dict: RepoInfo) -> Repo:
+        return cls(
+            origin=data_dict[ORIGIN_KEY],
+            mirror=data_dict[MIRROR_KEY],
+            path=Path(data_dict[PATH_KEY]),
+        )
+
+    def make_mirror(self) -> None:
+        path = pathlib.Path.cwd() / self.path / self.name
+
+        if not path.exists():
+            os.system(f"git clone {self.mirror} {path} --recursive")
+            if self.origin:
+                os.system(f"git -C {path} remote add epitech {self.origin}")
+
+        if self.origin:
+            os.system(f"git -C {path} pull epitech main")
+        else:
+            os.system(f"git -C {path} pull")
+
+        if not ((path / ".gitattributes").is_file() or "stumper_" in self.name):
+            os.system(f"cp {TEMPLATE_PATH}/.gitattributes {path}")
+
+        os.system(f"git -C {path} push origin main")
+
+    def _clone_from_args(self) -> None:
+        if self.name in sys.argv:
+            self.make_mirror()
+
+    @property
+    def clone(self) -> HandlerFunc:
+        return (
+            self.make_mirror
+            if len(sys.argv) == 1 else
+            self._clone_from_args
+        )
+
+
+HandlerFunc = Callable[[Repo], None]
 
 
 def main():
-    os.makedirs(REPO_PATH, exist_ok=True)
     with open(REPO_LIST_PATH) as f:
-        content = json.load(f)
-    if len(sys.argv) == 1:
-        all_mirror(content)
-    else:
-        specific_mirror(content)
+        content: List[RepoInfo] = json.load(f)
+
+    for line in content:
+        repo = Repo.from_dict(line)
+        repo.make_mirror()
 
 
 if __name__ == '__main__':
