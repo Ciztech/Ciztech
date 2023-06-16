9c12bf4626f8bfcc867589aa2574e9c15448a1df
Sigmanificient <edhyjox@gmail.com>
2023-06-17 01:02:36 +0200

    :zap: Use asynchronous call to slightly improve speed

--- free_repositories.py
+++ free_repositories.py
@@ -1,13 +1,16 @@
 from __future__ import annotations
 
-import os
+import asyncio
 import json
-import sys
 import pathlib
+import sys
+
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Final, TypedDict, List, Callable
-
+from time import perf_counter
+from typing import (
+    Final, TypedDict, List, Callable, Tuple, Coroutine, Any, Optional
+)
 
 PATH: Final[Path] = pathlib.Path.cwd()
 
@@ -20,17 +23,27 @@ MIRROR_KEY: Final[str] = "mirror"
 PATH_KEY: Final[str] = "path"
 
 
+async def run(cmd: str) -> Tuple[bytes, bytes]:
+    proc = await asyncio.create_subprocess_shell(
+        cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE
+    )
+
+    return await proc.communicate()
+
+
 class RepoInfo(TypedDict):
     path: str
-    origin: str
     mirror: str
+    origin: Optional[str]
 
 
 @dataclass
 class Repo:
     path: Path
-    origin: str
     mirror: str
+    origin: Optional[str] = None
 
     @property
     def name(self) -> str:
@@ -45,35 +58,38 @@ class Repo:
     @classmethod
     def from_dict(cls, data_dict: RepoInfo) -> Repo:
         return cls(
-            origin=data_dict[ORIGIN_KEY],
+            origin=data_dict.get(ORIGIN_KEY),
             mirror=data_dict[MIRROR_KEY],
             path=Path(data_dict[PATH_KEY]),
         )
 
-    def make_mirror(self) -> None:
+    async def make_mirror(self) -> None:
         path = pathlib.Path.cwd() / self.path / self.name
 
         if not path.exists():
-            os.system(f"git clone {self.mirror} {path} --recursive")
-            if self.origin:
-                os.system(f"git -C {path} remote add epitech {self.origin}")
+            print(f"Cloning {self.name}")
+            await run(f"git clone {self.mirror} {path} --recursive")
 
-        if self.origin:
-            os.system(f"git -C {path} pull epitech main")
-        else:
-            os.system(f"git -C {path} pull")
+            if self.origin is not None:
+                print(f"Registering remote: {self.name} -> {self.origin}")
+                await run(f"git -C {path} remote add epitech {self.origin}")
+
+        print(f"Pulling: {self.name}")
+        await run(f"git -C {path} pull" + (" epitech main" * bool(self.origin)))
 
         if not ((path / ".gitattributes").is_file() or "stumper_" in self.name):
-            os.system(f"cp {TEMPLATE_PATH}/.gitattributes {path}")
+            print(f"Add gitattributes in {self.name}")
+            await run(f"cp {TEMPLATE_PATH}/.gitattributes {path}")
 
-        os.system(f"git -C {path} push origin main")
+        print(f"Pushing on: {self.name}")
+        await run(f"git -C {path} push origin main")
 
-    def _clone_from_args(self) -> None:
+    async def _clone_from_args(self) -> None:
         if self.name in sys.argv:
-            self.make_mirror()
+            await self.make_mirror()
 
     @property
-    def clone(self) -> HandlerFunc:
+    def clone(self) -> Callable[[], Coroutine[Any, Any, None]]:
         return (
             self.make_mirror
             if len(sys.argv) == 1 else
@@ -81,17 +97,18 @@ class Repo:
         )
 
 
-HandlerFunc = Callable[[Repo], None]
-
-
-def main():
+async def main():
     with open(REPO_LIST_PATH) as f:
         content: List[RepoInfo] = json.load(f)
 
-    for line in content:
-        repo = Repo.from_dict(line)
-        repo.make_mirror()
+    marker = perf_counter()
+    await asyncio.gather(
+        *(Repo.from_dict(line).make_mirror() for line in content)
+    )
+
+    end = perf_counter()
+    print(f"Took {(end - marker):.3f}s.")
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
